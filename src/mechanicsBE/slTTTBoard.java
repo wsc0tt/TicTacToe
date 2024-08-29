f7b299093e41437d978c612ec7e832e7e3ffeffc
William Scott <wsc0tt@outlook.com>
2024-08-29 12:15:05 -0700

    printBoard,getMove,prompt methods complete

--- src/mechanicsBE/slTTTBoard.java
+++ src/mechanicsBE/slTTTBoard.java
@@ -1,4 +1,5 @@
 package mechanicsBE;
+import java.util.*;
 
 public class slTTTBoard {
     public char[][] board = {
@@ -9,13 +10,32 @@ public class slTTTBoard {
 
     // method to prompt user
     public void prompt() {
-
+        System.out.print("Enter row col numbers seperated by exactly one space (or type q to quit): ");
     }
 
 
-    // Get user input for next move
-    public void getMove() {
-
+    // Get user input for next move or quit
+    public String getMove() {
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        switch (line) {
+            case "0 0":
+            case "0 1":
+            case "0 2":
+            case "1 0":
+            case "1 1":
+            case "1 2":
+            case "2 0":
+            case "2 1":
+            case "2 2":
+                return line;
+            case "q":
+            case "Q":
+                return "quit";
+            default:
+                System.out.println("invalid entry - try again");
+                return "error";
+        }
     }
 
     // Mark the board
@@ -23,9 +43,11 @@ public class slTTTBoard {
 
     }
 
-    // method to check board for game over or continue
-    public void checkBoard(){
-
+    // method to check board for 3 in a row or full board
+    public void checkBoard(int move){
+        switch (move) {
+            case(0):
+        }
     }
 
     // method to print the board
@@ -40,8 +62,18 @@ public class slTTTBoard {
         System.out.println();
     }
 
-    // method to start a new tic tac toe game
+    // method to start a new tic-tac-toe game
     public void play() {
+        String move = "";               // init empty string to store the current move
+        while (!move.equals("quit")) {  // while the move is not equal to quit loop this
+            prompt();                   // ask the user for move
+            move = getMove();           // save the move
+            if (!move.equals("error") && !move.equals("quit")) {    // if the move is not an error or quit
+                //checkBoard(move);                                 //check the board then mark the board
+                //setBoard();
+                printBoard();
+            }
+        }
 
     }
 }

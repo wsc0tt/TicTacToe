4617296525f3df8589c9bfbcdc148bd358f3ff0b
William Scott <wsc0tt@outlook.com>
2024-08-28 20:48:07 -0700

    first commit

---
+++ src/csc133/csc133Driver.java
@@ -0,0 +1,15 @@
+package csc133;
+
+import mechanicsBE.slTTTBoard;
+
+public class csc133Driver {
+    public static void main(String[] args) {
+        slTTTBoard my_board = new slTTTBoard();
+
+        my_board.printBoard();
+        my_board.play();
+
+        System.out.println("Thank you for playing! Come back to waste more of your time!");
+    }
+}  // public class csc133Driver
+
---
+++ src/mechanicsBE/slTTTBoard.java
@@ -0,0 +1,5 @@
+package mechanicsBE;
+
+public class slTTTBoard {
+
+}

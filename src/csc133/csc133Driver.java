92329600679a266d041113eafc6b3ae100fe85c1
William Scott <wsc0tt@outlook.com>
2024-08-29 08:30:34 -0700

    Second commit

--- src/csc133/csc133Driver.java
+++ src/csc133/csc133Driver.java
@@ -7,7 +7,7 @@ public class csc133Driver {
         slTTTBoard my_board = new slTTTBoard();
 
         my_board.printBoard();
-        my_board.play();
+        //my_board.play();
 
         System.out.println("Thank you for playing! Come back to waste more of your time!");
     }
--- src/mechanicsBE/slTTTBoard.java
+++ src/mechanicsBE/slTTTBoard.java
@@ -1,5 +1,47 @@
 package mechanicsBE;
 
 public class slTTTBoard {
+    public char[][] board = {
+            {'-','-','-'},
+            {'-','-','-'},
+            {'-','-','-'}
+    };
 
+    // method to prompt user
+    public void prompt() {
+
+    }
+
+
+    // Get user input for next move
+    public void getMove() {
+
+    }
+
+    // Mark the board
+    public void setBoard() {
+
+    }
+
+    // method to check board for game over or continue
+    public void checkBoard(){
+
+    }
+
+    // method to print the board
+    public void printBoard() {
+        System.out.println();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    // method to start a new tic tac toe game
+    public void play() {
+
+    }
 }
